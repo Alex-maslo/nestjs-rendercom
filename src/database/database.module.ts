645ced1aebc23e82bd90aa5cb33f0e2e2d7c0a55
Olexandr <4439897@gmail.com>
2025-07-05 13:31:31 +0300

    feat: add PostgreSQL with SSL config for Render deployment

---
+++ src/database/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule], // щоб ConfigService був доступний
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        url: config.get<string>('DATABASE_URL'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
